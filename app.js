81ca8ed26de9031bf5bc3edd56f6b1096ff23fb1
Lucas Neumann <lucasneumann.fau@gmail.com>
2017-05-24 00:24:53 +0000

    add env variables

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var request = require('request');
 var app = express();
+
+var airtableKey = process.env.AIRTABLE_KEY;
+var airtableProductsUrl = process.env.AIRTABLE_PRODUCTS_URL;
+
 app.set('view engine', 'ejs')
 
 app.listen(process.env.PORT, process.env.IP, function(){
@@ -12,10 +16,14 @@ app.get('/', function(req, res){
 });
 
 
+// app.get('/:section/:subsection', function(req, res){
+//   var sec
+// })
+
 app.get('/linhas/:linha', function(req, res) {
   var linha = req.params.linha;
-  console.log(linha);
-  request('https://api.airtable.com/v0/appswoobu90DjfHdO/Produtos?api_key=key8KIoDLtssz0g54&filterByFormula={linha}="'+ linha + '"' , function(error, response, body){
+
+  request(airtableProductsUrl + '&filterByFormula={linha}="'+ linha + '"' , function(error, response, body){
     var parsedData = JSON.parse(body);
     if(!error && response.statusCode == 200 && parsedData['records'].length !=0){
       var parsedData = JSON.parse(body);
@@ -31,7 +39,7 @@ app.get('/linhas/:linha', function(req, res) {
 
 app.get('/categorias/:categoria', function(req, res) {
   var categoria = req.params.categoria;
-  request('https://api.airtable.com/v0/appswoobu90DjfHdO/Produtos?api_key=key8KIoDLtssz0g54&filterByFormula={categoria}="'+ categoria + '"' , function(error, response, body){
+  request(airtableProductsUrl + '&filterByFormula={categoria}="'+ categoria + '"' , function(error, response, body){
     var parsedData = JSON.parse(body);
     if(!error && response.statusCode == 200 && parsedData['records'].length !=0){
       res.render('categoria', {
@@ -49,8 +57,7 @@ app.get('/categorias/:categoria/:produto', function(req, res) {
   var linha = null
   var categoria = req.params.categoria
   var produto = req.params.produto
-  var url = 'https://api.airtable.com/v0/appswoobu90DjfHdO/Produtos?api_key=key8KIoDLtssz0g54&filterByFormula={Código}="'+ produto + '"'
-  console.log(url);
+  var url = airtableProductsUrl + '&filterByFormula={Código}="'+ produto + '"'
   request(url , function(error, response, body){
     var parsedData = JSON.parse(body);
     if(!error && response.statusCode == 200 && parsedData['records'].length !=0){
@@ -64,11 +71,6 @@ app.get('/categorias/:categoria/:produto', function(req, res) {
     } else
     res.render('404');
   });
-
-  // res.render('produto', {
-  //   produto: produto,
-  //   categoria: categoria
-  // })
 });
 
 app.get('/linhas/:linha/:produto', function(req, res) {
@@ -84,7 +86,8 @@ app.get('/linhas/:linha/:produto', function(req, res) {
   })
 });
 
+
+
 app.get('*', function(req, res) {
   res.render('404');
 })
-

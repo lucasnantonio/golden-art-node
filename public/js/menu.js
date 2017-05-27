8a38d7928c612153dc8e220cab7a14dec3984489
Lucas Neumann <lucasneumann.fau@gmail.com>
2017-05-27 18:18:15 +0000

    mega menu mobile

--- public/js/menu.js
+++ public/js/menu.js
@@ -8,19 +8,33 @@ function ready(fn) {
 
 var loadMenu = function(){
 
-    var menuWrapper = document.getElementById('menuWrapper');
-    var menuItems = document.getElementById('menuItems');
-    menuWrapper.style.height = '4.5rem';
+    var desktopMenuWrapper = document.getElementById('desktopMenuWrapper');
+    var desktopMenuItems = document.getElementById('desktopMenuItems');
+    var mobileMenuWrapper = document.getElementById('mobileMenuWrapper');
+    var mobileMenuItems = document.getElementById('mobileMenuItems');
+    var mobileMenuButton = document.getElementById('mobileMenuButton');
+
+    mobileMenuItems.style.height = "0px";
+
+    mobileMenuButton.addEventListener("click", function( event ) {
     
-    menuItems.addEventListener("mouseenter", function( event ) {   
-    menuWrapper.style.height = '15rem';
-  	menuWrapper.addEventListener("mouseleave", function(event){
-    setTimeout(function() {
-		menuWrapper.style.height = '4.5rem';
-    }, 500);
-    })
-  	
-}, false);
+      if (mobileMenuItems.style.height == "0px"){
+        mobileMenuItems.style.height = "auto";
+      } else {
+        mobileMenuItems.style.height = "0px";
+      }
+      
+    });
+    
+    desktopMenuWrapper.style.height = '4.5rem';
+    desktopMenuItems.addEventListener("mouseenter", function( event ) {   
+      desktopMenuWrapper.style.height = '15rem';
+    	desktopMenuWrapper.addEventListener("mouseleave", function(event){
+        setTimeout(function() {
+      		desktopMenuWrapper.style.height = '4.5rem';
+          }, 500);
+        })
+    });
 }
 
 ready(loadMenu);

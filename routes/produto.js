48efb76d33a33e051711adea4df4b7274a99bc30
Lucas Neumann <lucasneumann.fau@gmail.com>
2017-05-28 16:31:06 +0000

    async colors works!

--- routes/produto.js
+++ routes/produto.js
@@ -6,19 +6,55 @@ var nakedString = require("naked-string");
 
 
 // PRODUTOS/PRODUTO ROUTE
+router.get('/produtos/:produto', getProductInfo, findColorMatches, function(req, res) {
+  // console.log(res.locals.productColors);
+  res.render('produto', {data: res.locals.data, colors: res.locals.colors});
+  res.end();
+});
 
-router.get('/produtos/:produto', function(req, res) {
-  
+
+//middleware
+function getProductInfo(req, res, next){
   var produto = req.params.produto
-  var data;
-  
   base('Produtos').select({
     filterByFormula: "{Código} = '" +  produto + "'"
   }).firstPage(function(err, records) {
       if (err || !records[0]) { res.render('404'); return; }
-      data = records[0]["fields"];
-      res.render('produto', {data: data});
+      res.locals.data = records[0]["fields"];
+      next();
       });
-  });
+};
+
+// itemsProcessed++;
+//     if(itemsProcessed === array.length) {
+//       callback();
+//     }
+
+function callback () { console.log('all done'); }
+
+
+
+function findColorMatches(req, res, next){
+  
+  var itemsProcessed = 0;
+  res.locals.colors = [];
+  res.locals.data['Cores Pintura'].forEach(function(colorId, index, arr){
+    
+      base('Cores').find(colorId, function(err, record) {
+        if (err) { console.error(err); return; }
+        itemsProcessed++;
+        res.locals.colors.push(record);
+        console.log("element inserted");
+        if(itemsProcessed === arr.length) {
+          next();
+        };
+    });
+    });
+}
+
+  // console.log(res.locals.colors); 
+
+
+
 
 module.exports = router;

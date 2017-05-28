737e9ca5216f31563b04319b2391bd1dcc11bc54
Lucas Neumann <lucasneumann.fau@gmail.com>
2017-05-28 04:09:47 +0000

    siema carousel

---
+++ public/js/siema.js
@@ -0,0 +1,13 @@
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+var loadCarousel = function(){
+  new Siema(); /*global Siema*/
+};
+
+ready(loadCarousel);

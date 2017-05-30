96b984e42cdb7c1a73b87375359d3591a9ea473f
Lucas Neumann <lucasneumann.fau@gmail.com>
2017-05-30 03:43:14 +0000

    complete local JSON migration

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,5 @@
 var express = require('express');
 var router  = new express.Router({mergeParams: true});
-// var Airtable = require('airtable');
-
-// var base = new Airtable({apiKey: process.env.AIRTABLE_KEY}).base('appswoobu90DjfHdO');
 
 router.get('/', function(req, res) {
 res.render('home', {productsData: res.locals.productsData});
--- routes/produto.js
+++ routes/produto.js
@@ -7,74 +7,91 @@ var nakedString = require("naked-string");
 
 // PRODUTOS/PRODUTO ROUTE
 router.get('/produtos/:produto', 
-  getProductInfo, 
+  getProductInfo,
+  getColorsData,
   findColorMatches,
   findVariations,
   function(req, res) {
-  res.render('produto', {data: res.locals.data, colors: res.locals.colors, variations: res.locals.variations});
+  res.render('produto', {data: res.locals.thisProductData['fields'], colors: res.locals.thisProductColors, variations: res.locals.thisProductVariations});
   res.end();
 });
 
 
-//middleware
+
+function getColorsData(req, res, next){
+  console.log("hi");
+  var colorsData = [];
+  
+  base('Cores').select({}).eachPage(function page(records, fetchNextPage) {
+    records.forEach(function(item){
+      colorsData.push(item);
+    });
+    
+    fetchNextPage();
+    
+  }, function done(err) {
+    if (err) {res.render('404'); return; }
+    
+    res.locals.colorsData = colorsData;
+    next();
+
+  });
+  
+}
+
+
 function getProductInfo(req, res, next){
   var produto = req.params.produto
-  base('Produtos').select({
-    filterByFormula: "{Código} = '" +  produto + "'"
-  }).firstPage(function(err, records) {
-      if (err || !records[0]) { res.render('404'); return; }
-      console.log(records)
-      res.locals.data = records[0]["fields"];
+  
+  res.locals.productsData.forEach(function(product){
+    if (product['fields']['Código'] == produto){
+
+      res.locals.thisProductData = product;
       next();
-      });
+    }
+  })
 };
 
 
 function findColorMatches(req, res, next){
-  
-  var itemsProcessed = 0;
-  res.locals.colors = [];
-  
-  if (res.locals.data['Cores Pintura']){
-  res.locals.data['Cores Pintura'].forEach(function(colorId, index, arr){
-    
-      base('Cores').find(colorId, function(err, record) {
-        if (err) { console.error(err); return; }
-        itemsProcessed++;
-        res.locals.colors.push(record);
+  res.locals.thisProductColors = [];
+  if(res.locals.thisProductData['fields']['Cores Pintura']){
 
-        if(itemsProcessed === arr.length) {
-          next();
-        };
-    });
-    });
-} else {
-  next()}
+  res.locals.colorsData.forEach(function(color, index, arr){
+      for (var i=0; i < res.locals.thisProductData['fields']['Cores Pintura'].length; i++){
+      if(res.locals.thisProductData['fields']['Cores Pintura'][i] == color['id']){
+      res.locals.thisProductColors.push(color)
+      }
+      }
+  });
+  next();
+  } else {
+next();
+}
 }
 
-//variations
 function findVariations(req, res, next){
+  res.locals.thisProductVariations = [];
   
-  var itemsProcessed = 0;
+  if(res.locals.thisProductData['fields']['Varições']){
   res.locals.variations = [];
+  console.log(res.locals.thisProductData['fields']['Variações']);
 
-  if (res.locals.data['Variações']){
-  res.locals.data['Variações'].forEach(function(variationId, index, arr){
+  res.locals.productsData.forEach(function(product, index, arr){
+      for (var i=0; i < res.locals.thisProductData['fields']['Variações'].length; i++){
+      if(res.locals.thisProductData['fields']['Varições'][i] == res.locals.productsData['id']){
+      res.locals.variations.push(product)
+      }
+      }
+  });
+  next();
+  
+  }else{
     
-      base('Produtos').find(variationId, function(err, record) {
-        if (err) { console.error(err); return; }
-        itemsProcessed++;
-        res.locals.variations.push(record);
-
-        if(itemsProcessed === arr.length) {
-          console.log(res.locals.variations);
-          next();
-        };
-    });
-    });
-} else {
-  next()}
+next();
 }
+}
+
 
 //TODO: special colors
 //TODO: cúpulas

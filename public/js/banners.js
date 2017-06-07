bee5e3dfaae639caf07b134950d766e434845032
Lucas Neumann <lucas.neumann@nubank.com.br>
2017-06-06 22:42:14 -0300

    changes slider to siema

--- public/js/banners.js
+++ public/js/banners.js
@@ -1,24 +1,24 @@
-function ready(fn) {
-  if (document.readyState != 'loading'){
-    fn();
-  } else {
-    document.addEventListener('DOMContentLoaded', fn);
-  }
-}
-
-var bannerAnimations = function(){
-
-	var banners = document.querySelectorAll('.banner')
-	console.log(banners)
-
-	for (i = 0; i < banners.length; i++) {
-		banners[i].addEventListener('mouseenter', function(ev){
-			ev.target.classList.add('flexGrow')
-		})
-		banners[i].addEventListener('mouseleave', function(ev){
-			ev.target.classList.remove('flexGrow')
-		})
-	}
-}
+// function ready(fn) {
+//   if (document.readyState != 'loading'){
+//     fn();
+//   } else {
+//     document.addEventListener('DOMContentLoaded', fn);
+//   }
+// }
+//
+// var bannerAnimations = function(){
+//
+// 	var banners = document.querySelectorAll('.banner')
+// 	console.log(banners)
+//
+// 	for (i = 0; i < banners.length; i++) {
+// 		banners[i].addEventListener('mouseenter', function(ev){
+// 			ev.target.classList.add('flexGrow')
+// 		})
+// 		banners[i].addEventListener('mouseleave', function(ev){
+// 			ev.target.classList.remove('flexGrow')
+// 		})
+// 	}
+// }
 
 // ready(bannerAnimations);

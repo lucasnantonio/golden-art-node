760e745d648baaed7100fff6e1bbc93db694a7fb
lucasnantonio <lucasneumann.fau@gmail.com>
2020-01-19 22:52:25 -0500

    lots of changes

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,47 +1,47 @@
 function ready(fn) {
-  if (document.readyState != 'loading'){
+  if (document.readyState != 'loading') {
     fn();
   } else {
     document.addEventListener('DOMContentLoaded', fn);
   }
 }
 
-var loadMenu = function(){
+var loadMenu = function () {
 
-    var logo = document.getElementById('logo');
-    var logoSmall = document.getElementById('logo-small');
-    var logoExpanded = document.getElementById('logo-expanded');
+  var logo = document.getElementById('logo');
+  var logoSmall = document.getElementById('logo-small');
+  var logoExpanded = document.getElementById('logo-expanded');
 
-    var mobileMenuWrapper = document.getElementById('mobileMenuWrapper');
-    var mobileMenuItems = document.getElementById('mobileMenuItems');
-    var mobileMenuButton = document.getElementById('mobileMenuButton');
+  var mobileMenuWrapper = document.getElementById('mobileMenuWrapper');
+  var mobileMenuItems = document.getElementById('mobileMenuItems');
+  var mobileMenuButton = document.getElementById('mobileMenuButton');
 
-    mobileMenuItems.style.height = "0px";
-    logoExpanded.style.left = '-10rem';
-    logoExpanded.style.opacity = 0;
+  mobileMenuItems.style.height = "0px";
+  // logoExpanded.style.left = '-10rem';
+  // logoExpanded.style.opacity = 0;
 
-    mobileMenuButton.addEventListener("click", function( event ) {
+  mobileMenuButton.addEventListener("click", function (event) {
 
-      if (mobileMenuItems.style.height == "0px"){
-        mobileMenuWrapper.style.backgroundColor = 'white';
-        mobileMenuButton.style.color = 'gray';
-        mobileMenuItems.style.height = "100vh";
-      } else {
-        mobileMenuWrapper.style.backgroundColor = 'transparent';
-        mobileMenuItems.style.height = "0px";
-        mobileMenuButton.style.color = 'white';
-      }
+    if (mobileMenuItems.style.height == "0px") {
+      mobileMenuWrapper.style.backgroundColor = 'white';
+      mobileMenuButton.style.color = 'gray';
+      mobileMenuItems.style.height = "100vh";
+    } else {
+      mobileMenuWrapper.style.backgroundColor = 'transparent';
+      mobileMenuItems.style.height = "0px";
+      mobileMenuButton.style.color = 'white';
+    }
 
-    });
+  });
 
-    logo.addEventListener("mouseenter", function( event ) {
-      logoExpanded.style.left = '0';
-      logoExpanded.style.opacity = 1;
-    });
-    logo.addEventListener("mouseleave", function( event ) {
-      logoExpanded.style.left = '-10rem'
-      logoExpanded.style.opacity = 0;
-    });
+  // logo.addEventListener("mouseenter", function( event ) {
+  //   logoExpanded.style.left = '0';
+  //   logoExpanded.style.opacity = 1;
+  // });
+  // logo.addEventListener("mouseleave", function( event ) {
+  //   logoExpanded.style.left = '-10rem'
+  //   logoExpanded.style.opacity = 0;
+  // });
 }
 
 ready(loadMenu);
